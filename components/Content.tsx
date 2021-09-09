c726ed1c1953c7d1633852b613bf61a3f229dcc1
JFranciscoRobles <josefrancisco.roblesacosta@gmail.com>
2021-09-09 13:12:48 -0700

    commentprops

--- components/Content.tsx
+++ components/Content.tsx
@@ -9,10 +9,10 @@ function Content({ text }: Props): ReactElement {
   return (
     <Fragment>
       <Title>Title</Title>
-      <SubTitle $active $size="lg">
+      <SubTitle $active $size={"lg"}>
         {text}
       </SubTitle>
-      <SubTitle $size={"sm"}>{text}</SubTitle>
+      <SubTitle $size="sm">{text}</SubTitle>
     </Fragment>
   );
 }
@@ -27,19 +27,29 @@ const Title = tw.h1`
 
 //Sizes config
 //SubTitle Config Props
+
 type Size = "sm" | "normal" | "lg";
+type SubTitleProps = {
+  /**
+   *"true | false".
+   */
+  $active?: boolean;
+  /**
+   *"sm | normal | lg".
+   */
+  $size?: Size;
+};
+
 const sizes: Record<Size, any> = {
   sm: `text-sm`,
   normal: `text-base`,
   lg: `text-lg`,
 };
 
-type SubTitleProps = {
-  $active?: boolean;
-  $size?: Size;
-};
-
 //Styled Div with Config Props
+/**
+ * @props active, size
+ */
 const SubTitle = tw.div<SubTitleProps>`
 ${(p) => (p.$active ? "text-green-800" : "text-red-800")}
 ${({ $size }) => $size && `${sizes[$size]}`}
