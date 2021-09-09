574f7dd805c45c8b2c6ba5dbf5c82d272765a070
JFranciscoRobles <josefrancisco.roblesacosta@gmail.com>
2021-09-09 11:43:44 -0700

    setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,34 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`
+import tw from "tailwind-styled-components";
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <Container>
+      <Title>Title</Title>
+      <SubTitle $active>SubTitle</SubTitle>
+      <SubTitle $active={false}>SubTitleTwo</SubTitle>
+    </Container>
+  );
 }
+
+interface SubTitleProps {
+  $active: boolean;
+}
+
+const Container = tw.div`
+  bg-gray-50
+  flex
+  min-h-screen
+  items-center
+  justify-center
+  w-full
+  flex-col
+`;
+
+const Title = tw.h1`
+  text-red-500
+  text-4xl
+`;
+
+const SubTitle = tw.div<SubTitleProps>`
+${(p) => (p.$active ? "text-green-800" : "text-red-800")}
+`;
