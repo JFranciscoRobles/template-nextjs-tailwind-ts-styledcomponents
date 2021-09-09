bb719e2551069fd7433f74d6d339425b4dfd9eb5
JFranciscoRobles <josefrancisco.roblesacosta@gmail.com>
2021-09-09 12:42:16 -0700

    examplesprops

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment, ReactElement } from "react";
 import tw from "tailwind-styled-components";
+
 interface Props {
   text: string;
 }
@@ -8,23 +9,38 @@ function Content({ text }: Props): ReactElement {
   return (
     <Fragment>
       <Title>Title</Title>
-      <SubTitle $active>{text}</SubTitle>
-      <SubTitle $active={false}>{text}</SubTitle>
+      <SubTitle $active $size="lg">
+        {text}
+      </SubTitle>
+      <SubTitle $size={"sm"}>{text}</SubTitle>
     </Fragment>
   );
 }
 
 export default Content;
 
-type SubTitleProps = {
-  $active: boolean;
-};
-
+//Title TWCSS
 const Title = tw.h1`
   text-red-500
   text-4xl
 `;
 
+//Sizes config
+//SubTitle Config Props
+type Size = "sm" | "normal" | "lg";
+const sizes: Record<Size, any> = {
+  sm: `text-sm`,
+  normal: `text-base`,
+  lg: `text-lg`,
+};
+
+type SubTitleProps = {
+  $active?: boolean;
+  $size?: Size;
+};
+
+//Styled Div with Config Props
 const SubTitle = tw.div<SubTitleProps>`
 ${(p) => (p.$active ? "text-green-800" : "text-red-800")}
+${({ $size }) => $size && `${sizes[$size]}`}
 `;
